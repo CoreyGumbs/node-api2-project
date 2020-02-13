9f6c1238b6a8f5feb31066e8ffd4235b3d205073
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-13 14:57:41 -0500

    [Add] Added delete end point, tested/w postman, working

--- routers/posts.js
+++ routers/posts.js
@@ -135,5 +135,23 @@ router.put('/:id', (req, res) => {
     }
 });
 //DELETE ROUTES
+router.delete('/:id', (req, res) =>{
+    const {id} = req.params;
+
+    db.findById(id)
+    .then(post => {
+        if(post.length === 0){
+            res.status(404).json({message: "The post with the specified ID does not exist." });
+        }else{
+            db.remove(id)
+            .then(post => {
+                res.status(204).json({message: "Post deleted."});
+            });    
+        }
+    })
+    .catch(error => {
+        res.status(500).json({error: "The post could not be removed" });
+    })
+})
 
 module.exports = router;
